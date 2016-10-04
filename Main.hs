db98c07961313eafc8905f2052adff96caf7ab20
Dave Compton <davecompton7@gmail.com>
2016-10-04 20:16:12 +0000

    changed some names

--- Main.hs
+++ Main.hs
@@ -8,18 +8,17 @@ import GHCJS.DOM.EventM (mouseOffsetXY)
 
 type Point = (Double,Double)
 
-data Ball  = Ball {
-                     position :: Point,
-                     velocity :: Point,
-                     radius   :: Double,
-                     color    :: Text
+data Ball  = Ball { position :: Point,
+                    velocity :: Point,
+                    radius   :: Double,
+                    color    :: Text
                   } deriving (Eq, Ord)
 
 svgNamespace :: Maybe Text
 svgNamespace = (Just "http://www.w3.org/2000/svg")
 
-addPick :: (Int,Int) -> Map (Int, Ball) () -> Map (Int,Ball) ()
-addPick (x,y) cs = 
+addBall :: (Int,Int) -> Map (Int, Ball) () -> Map (Int,Ball) ()
+addBall (x,y) cs = 
     let position = (fromIntegral x, fromIntegral y)
         velocity = (0.0,0.0)
         radius = 10.0
@@ -27,18 +26,12 @@ addPick (x,y) cs =
         ball = Ball position velocity radius color
     in insert (length cs, ball) () cs
 
-showCircle :: MonadWidget t m => (Int,Ball) -> Dynamic t () -> m () 
-showCircle (index, Ball (x,y) _ radius color ) _  = do
-    let 
-        t_x =      pack $ show x
-        t_y =      pack $ show y
-        t_radius = pack $ show radius
-        t_style  = "fill:" `DT.append` color
-
-        circleAttrs = fromList [ ( "cx",     t_x)
-                               , ( "cy",     t_y)
-                               , ( "r",      t_radius)
-                               , ( "style",  t_style) ] 
+showBall :: MonadWidget t m => (Int,Ball) -> Dynamic t () -> m () 
+showBall (index, Ball (x,y) _ radius color ) _  = do
+    let circleAttrs = fromList [ ( "cx",     pack $ show x)
+                               , ( "cy",     pack $ show y)
+                               , ( "r",      pack $ show radius)
+                               , ( "style",  "fill:" `DT.append` color) ] 
 
     elDynAttrNS' svgNamespace "circle" (constDyn circleAttrs) $ return ()
     return ()
@@ -48,20 +41,19 @@ main = mainWidget $ do
     let attrs =   constDyn $ 
                       fromList 
                           [ ("width" , "500")
-                          , ("height" , "250")
+                          , ("height", "400")
                           , ("style" , "border:solid; margin:8em")
                           ]
     rec 
-        let circles = listWithKey picks showCircle
-
-        (elm, _) <-   elDynAttrNS' svgNamespace "svg" attrs circles
+        (elm, _) <- elDynAttrNS' svgNamespace "svg" 
+                        attrs $ listWithKey balls showBall 
 
         mouseEvent <- wrapDomEvent 
                           (_element_raw elm) 
                           (onEventName Mousedown) 
                           mouseOffsetXY
 
-        picks <- foldDyn addPick empty mouseEvent
+        balls <- foldDyn addBall empty mouseEvent
 
     return ()
 
