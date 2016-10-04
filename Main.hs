32c5f794752cacbf546b200998ce4f89c8c217e5
Dave Compton <davecompton7@gmail.com>
2016-10-04 16:14:52 +0000

    changed some Ints to Doubles

--- Main.hs
+++ Main.hs
@@ -6,24 +6,24 @@ import Data.Map (Map, fromList, insert, empty)
 import Data.Text as DT (Text, pack, append)
 import GHCJS.DOM.EventM (mouseOffsetXY) 
 
-type Point = (Int,Int)
+type Point = (Double,Double)
 
 data Ball  = Ball {
                      position :: Point,
                      velocity :: Point,
-                     radius   :: Int,
+                     radius   :: Double,
                      color    :: Text
                   } deriving (Eq, Ord)
 
 svgNamespace :: Maybe Text
 svgNamespace = (Just "http://www.w3.org/2000/svg")
 
-drawCircle :: MonadWidget t m => Text -> Int -> Int -> Int -> m ()
+drawCircle :: MonadWidget t m => Text -> Double -> Double -> Double -> m ()
 drawCircle color radius x y = do
     let 
-        t_x =      pack $ show (fromIntegral x :: Double)
-        t_y =      pack $ show (fromIntegral y :: Double)
-        t_radius = pack $ show (fromIntegral radius :: Double)
+        t_x =      pack $ show x
+        t_y =      pack $ show y
+        t_radius = pack $ show radius
         t_style  = "fill:" `DT.append` color
 
         circleAttrs = fromList [ ( "cx",     t_x)
@@ -34,8 +34,8 @@ drawCircle color radius x y = do
     elDynAttrNS' svgNamespace "circle" (constDyn circleAttrs) $ return ()
     return ()
 
-addPick :: Point -> Map (Int, Ball) () -> Map (Int,Ball) ()
-addPick point cs  = let ball = Ball point (0,0) 10 "Red" in insert (length cs, ball) () cs
+addPick :: (Int,Int) -> Map (Int, Ball) () -> Map (Int,Ball) ()
+addPick (x,y) cs  = let ball = Ball (fromIntegral x, fromIntegral y) (0.0,0.0) 10.0 "Red" in insert (length cs, ball) () cs
 
 showCircle :: MonadWidget t m => (Int,Ball) -> Dynamic t () -> m () 
 showCircle (index, Ball (x,y) _ radius color ) _  = drawCircle color radius x y
