6385029658b340b543d3bd78a9d659050e27fc80
Dave Compton <davecompton7@gmail.com>
2016-10-05 03:32:20 +0000

    introduced svg selection (does not work yet )

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE RecursiveDo #-}
 import Reflex
 import Reflex.Dom 
-import Data.Map (Map, fromList, toList, insert, empty)
+import Data.Map (Map, fromList, toList, insert, empty, elems)
 import Data.Text as DT (Text, pack, append)
 import GHCJS.DOM.EventM (mouseOffsetXY) 
 import Data.Time.Clock (NominalDiffTime, getCurrentTime)
@@ -20,7 +20,13 @@ minus (x0,y0) (x1, y1) = (x0-x1, y0-y1)
 bounce :: (Double, Double) -> (Double, Double) 
 bounce (x0,y0) = (x0, -y0)
 
-data Cmd = Tick | Pick (Int, Int)
+height = 400
+width = 600
+
+vflip :: Double -> Double
+vflip y = fromIntegral height - y
+
+data Cmd = Tick | Pick (Int, Int) | Poke Int
 
 data Ball  = Ball { position :: Point,
                     velocity :: Vector,
@@ -48,45 +54,56 @@ fall ((i, b),()) =
 
 update :: Cmd -> Map (Int, Ball) () -> Map (Int,Ball) ()
 update (Pick (x,y)) cs = 
-    let position = (fromIntegral x, fromIntegral y)
+    let position = (fromIntegral x, vflip $ fromIntegral y)
         velocity = (0.0,0.0)
-        radius = 10.0
+        radius = 50.0
         color = "Green"
         ball = Ball position velocity radius color
     in insert (length cs, ball) () cs
 
-update Tick cs = fromList $ fmap fall $ toList cs
+update Tick cs = 
+    fromList $ fmap fall $ toList cs
 
-showBall :: MonadWidget t m => (Int,Ball) -> Dynamic t () -> m () 
+update (Poke index) cs = 
+    -- fromList $ filter (\((i,_),_) -> i /= index) $ toList cs
+    fromList []
+
+showBall :: MonadWidget t m => (Int,Ball) -> Dynamic t () -> m (Event t Cmd)
 showBall (index, Ball (x,y) _ radius color ) _  = do
     let circleAttrs = fromList [ ( "cx",     pack $ show x)
-                               , ( "cy",     pack $ show y)
+                               , ( "cy",     pack $ show $ vflip y)
                                , ( "r",      pack $ show radius)
                                , ( "style",  "fill:" `DT.append` color) ] 
 
-    elDynAttrNS' svgNamespace "circle" (constDyn circleAttrs) $ return ()
-    return ()
+    (el,_) <- elDynAttrNS' svgNamespace "circle" (constDyn circleAttrs) $ return ()
+    return $ fmap (const $ Poke index) $ domEvent Click el
 
+showBalls :: MonadWidget t m => Dynamic t (Map (Int,Ball) ()) -> m (Event t Cmd)
+showBalls balls = do
+        pokeEventWithKeys <- listWithKey balls showBall
+        let pokeEvent = switch $ (leftmost . elems) <$> current pokeEventWithKeys
+        return pokeEvent
 
 main = mainWidget $ do 
     tick <- tickLossy  updateFrequency =<< liftIO getCurrentTime
     let attrs =   constDyn $ 
                       fromList 
-                          [ ("width" , "500")
-                          , ("height", "400")
+                          [ ("width" , pack $ show width)
+                          , ("height", pack $ show height)
                           , ("style" , "border:solid; margin:8em")
                           ]
     rec 
-        (elm, _) <- elDynAttrNS' svgNamespace "svg" 
-                        attrs $ listWithKey balls showBall 
+        (elm, ev) <- elDynAttrNS' svgNamespace "svg" 
+                         attrs $ showBalls balls
 
         mouseEvent <- wrapDomEvent 
                           (_element_raw elm) 
                           (onEventName Mousedown) 
                           mouseOffsetXY
 
-        let events = leftmost [fmap Pick mouseEvent, 
-                               fmap (const Tick) tick]
+        let events = leftmost [ev, 
+                               fmap Pick mouseEvent, 
+                               fmap (const Tick) tick ]
 
         balls <- foldDyn update empty events
 
